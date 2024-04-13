89147d70ab5f62bfa57600b6b872d77b0991fb6e
IdaCy <ifc00007@students.stir.ac.uk>
2024-04-13 10:13:11 +0100

    static try

--- staticfiles/js/add_datacr.7c1b835045de.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    console.log("DOM content loaded");
-
-    // Generate a unique reference_id based on the date and time
-    var reference_id = new Date().toISOString().replace(/[-:TZ.]/g, '').slice(3, -5);
-    document.getElementById('ref_id').value = reference_id;
-    console.log("Reference ID set:", reference_id);
-});
